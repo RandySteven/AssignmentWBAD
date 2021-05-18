f495a32a5386211261b2036157ccb4df5595006e
unknown <randysteven12@gmail.com>
2021-05-18 17:55:59 +0700

    change

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+class User {
+    constructor(fullName, email, password){
+        this.fullName = fullName,
+        this.email = email,
+        this.password = password
+    }
+}
 
-function register(){
-    var fullName = document.forms["registerForm"]["fullName"].value
-    var email = document.forms["registerForm"]["email"].value
-    var password = document.forms["registerForm"]["password"].value
+var users = [];
+
+var fullName = document.forms["registerForm"]["fullName"].value
+var email = document.forms["registerForm"]["email"].value
+var password = document.forms["registerForm"]["password"].value
+
+function register(fullName, email, password){
+    if(fullName === undefined){
+        alert("Full Name tidak boleh kosong")
+        return false
+    }
     if(fullName.length < 5){
         alert("Full Name tidak boleh kurang dari 5")
         return false
@@ -11,8 +25,24 @@ function register(){
         alert("Email tidak boleh kurang dari 5")
         return false
     }
-    if(email.indexOf("@") !== -1){
-        alert("Email tidak boleh kurang dari 5")
-        return false
-    }
+
+    let user = new User(fullName, email, password)
+    users.push(user)
+}
+
+console.log(users)
+
+function index(){
+    // var email = document.forms["loginForm"]["email"].value
+    // var password = document.forms["loginForm"]["password"].value
+    // for(let i = 0 ; i < users.length ; i++){
+    //     if(email === users[i].email && password === users[i].password){
+    //         fullName = users[i].fullName;
+    //         console.log(fullName)
+    //         return fullName
+    //     }else{
+    //         alert(`Users not find`)
+    //         return false
+    //     }
+    // }
 }
